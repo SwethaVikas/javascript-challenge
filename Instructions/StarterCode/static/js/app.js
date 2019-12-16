8e77b5d43fe21233ca90c4b0602bfe9246fc7783
SwethaVikas <shwethavikas@gmail.com>
2019-12-15 21:01:00 -0500

    Final Commit

---
+++ Instructions/StarterCode/static/js/app.js
@@ -0,0 +1,59 @@
+
+// Created by Swetha Vikas
+
+// Assign the data from `data.js` to a descriptive variable
+var tableData = data;
+
+// UFO sightings function display
+function tableDisplay(ufoSightings) {
+    var tbody = d3.select("tbody");
+    ufoSightings.forEach((ufoRecord) => {
+      var row = tbody.append("tr");
+      Object.entries(ufoRecord).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.html(value);
+      });
+    });
+  };
+  
+  // Table Clear
+  function deleteTbody() {
+    d3.select("tbody")
+      .selectAll("tr").remove()
+      .selectAll("td").remove();
+  };
+  
+  // initial display 
+  console.log(tableData);
+  tableDisplay(tableData);
+  
+  // Select the input element and get the raw HTML node
+  var button = d3.select("#filter-btn");
+  
+  // filter 
+  button.on("click", function(event) {
+    d3.event.preventDefault();
+    deleteTbody();
+    var dateInput = d3.select("#datetime").property("value");
+    
+    if (dateInput.trim() === "" ) {
+      
+      var filteredData = tableData;
+    } else {
+      
+      var filteredData = tableData.filter(ufoSighting => 
+        ufoSighting.datetime === dateInput.trim());
+    };
+  
+    // Exception
+    if (filteredData.length == 0) {
+      d3.select("tbody")
+        .append("tr")
+        .append("td")
+          .attr("colspan", 7)
+          .html("<h4> Data Not Found</h4>");
+    };
+  
+    console.log(filteredData);
+    tableDisplay(filteredData);
+  });
